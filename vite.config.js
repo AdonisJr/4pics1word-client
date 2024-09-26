5d9ffb74e2d86e296d64c471ee08165129823633
unknown <adonisjr.suico@gmail.com>
2024-09-27 00:16:44 +0800

    chore: added base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: import.meta.env.VITE_BASE_URL,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
